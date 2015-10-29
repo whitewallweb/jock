f7e910e51b50b7c97954c3b8d41c817b63ceefc0
Martin Gouws <martin@whitewallweb.com>
2015-10-29 15:11:34 +0200

    cart on shop page

--- wp-content/themes/afgri_theme/woocommerce/archive-product.php
+++ wp-content/themes/afgri_theme/woocommerce/archive-product.php
@@ -49,7 +49,7 @@ get_header( 'shop' ); ?>
 				<ul class="shoploop">
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php wc_get_template_part( 'content', 'product' ); ?>
+					<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 				</ul>
--- wp-content/themes/afgri_theme/woocommerce/content-product.php
+++ wp-content/themes/afgri_theme/woocommerce/content-product.php
@@ -50,7 +50,7 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 			 */
 			do_action( 'woocommerce_before_shop_loop_item_title' );
 		?>
-
+         
 		<h3><?php the_title(); ?></h3>
 
 		<?php
@@ -68,3 +68,4 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 	<?php do_action( 'woocommerce_after_shop_loop_item' ); ?>
 
 </li>
+
