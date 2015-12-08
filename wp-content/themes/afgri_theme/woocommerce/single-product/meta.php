cf9e3a1e18648ed65f35f70bb6810361a6f962d2
Martin Gouws <martin@whitewallweb.com>
2015-12-08 15:52:34 +0200

    auto complete

--- wp-content/themes/afgri_theme/woocommerce/single-product/meta.php
+++ wp-content/themes/afgri_theme/woocommerce/single-product/meta.php
@@ -27,7 +27,7 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php endif; ?>
 
-	<?//php echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '.</span>' ); ?>
+	<?php// echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '.</span>' ); ?>
 
 	<?php echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . ' ', '.</span>' ); ?>
 
